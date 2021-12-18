7b5be197c2b1e4ce99b348a567c52514b275fde3
Pawel P <pawelprimus@gmail.com>
2021-12-18 22:50:01 +0100

    In work

--- src/primus/pawel/DAY_05/DAY_05.java
+++ src/primus/pawel/DAY_05/DAY_05.java
@@ -7,11 +7,17 @@ class DAY_05 {
     public static void main(String[] args) throws Exception {
         String[] input = readFileAsString("05").split("[\\r\\n]+");
         String[] points;
+        String[] point;
 
         for (int i = 0; i < input.length; i++) {
             points = input[i].split("->");
             for (int j = 0; j < points.length; j++) {
-                System.out.println(points[j]);
+                point = points[j].split(",");
+                for (int k = 0; k < po; k++) {
+                    
+                }
+                
+                
             }
 
 
