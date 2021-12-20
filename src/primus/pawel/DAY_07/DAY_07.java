4d72ce9ecb4bef03b1de483997a34c86f822a10f
Pawel P <pawelprimus@gmail.com>
2021-12-20 20:23:39 +0100

    Finished day 07 part 1

---
+++ src/primus/pawel/DAY_07/DAY_07.java
@@ -0,0 +1,47 @@
+package primus.pawel.DAY_07;
+
+import primus.pawel.InputType;
+
+import static primus.pawel.FileReader.readFileAsString;
+
+//https://adventofcode.com/2021/day/7
+class DAY_07 {
+
+    public static void main(String[] args) throws Exception {
+
+        String[] input = readFileAsString("07", InputType.ORIGINAL).split(",");
+        int[] initialState = new int[input.length];
+        int maximalHorizontalPosition = 0;
+        int minimalHorizontalPosition = 0;
+        int minimalFuel = Integer.MAX_VALUE;
+        int currentFuel = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            initialState[i] = Integer.parseInt(input[i].trim());
+            System.out.println(initialState[i]);
+            if (maximalHorizontalPosition < initialState[i]) {
+                maximalHorizontalPosition = initialState[i];
+            }
+        }
+
+        for (int i = minimalHorizontalPosition; i <= maximalHorizontalPosition; i++) {
+
+            for (int j = 0; j < initialState.length; j++) {
+
+                currentFuel += Math.abs(initialState[j] - i);
+
+            }
+
+            if (minimalFuel > currentFuel) {
+                minimalFuel = currentFuel;
+            }
+
+            System.out.println("Horizontal POS:" + i + " FUEL: " + currentFuel);
+            currentFuel = 0;
+        }
+
+        System.out.println("RESULT: " + minimalFuel);
+
+
+    }
+}
